7c345282f4b6388502c2a7025569b27a5a174d0a
BELLA INDRYANTI <20102063@ittelkom-pwt.ac.id>
2023-06-06 16:57:13 +0700

    bella

--- app/src/main/java/com/bella/fitassistai/WelcomeActivity.kt
+++ app/src/main/java/com/bella/fitassistai/WelcomeActivity.kt
@@ -1,11 +1,55 @@
 package com.bella.fitassistai
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.bella.fitassistai.databinding.ActivityWelcomeBinding
+import com.bella.fitassistai.detail.login.LoginActivity
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setView()
+        setAction()
+        setAnimation()
+    }
+
+    private fun setAction(){
+        binding.btnStarted.setOnClickListener {
+            startActivity(Intent(this@WelcomeActivity, LoginActivity::class.java))
+        }
+    }
+
+    private fun setView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setAnimation(){
+        val start = ObjectAnimator.ofFloat(binding.btnStarted, View.ALPHA, 1f).setDuration(500)
+
+        AnimatorSet().apply {
+            playSequentially(start)
+            start()
+        }
     }
 }
